f28706412b57a42990e054b3581f3bbfdea113b9
Oksiuk <yelyzaveta.oksiuk@gmail.com>
2020-12-11 00:30:53 +0200

    master branch created

--- 02_task.rb
+++
@@ -1,2 +0,0 @@
-# What is the equation to get 24 with 1,3,4 and 6 you can only divide, multiply or add them?
-24 == 6 * 4 + 1/3
--- 03_task.rb
+++
@@ -1,2 +0,0 @@
-# Get the number 2009 using arithmetic and eleven twos
-2222 - 222 + 22 / 2 - 2 == 2009
